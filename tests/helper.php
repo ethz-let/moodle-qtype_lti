1345b1dc5e19388f9c9c23da35976b0c376fd445
spillinge <76476553+spillinge@users.noreply.github.com>
2022-10-04 15:27:22 +0200

    Update helper.php
    fixed questiontext and generalfeedback, deleted unnecessary functions

--- tests/helper.php
+++ tests/helper.php
@@ -36,71 +36,6 @@ class qtype_lti_test_helper extends question_test_helper {
     }
 
     /**
-     * Helper method to reduce duplication.
-     *
-     * @return qtype_lti_question
-     */
-    public function initialise_lti_question() {
-        question_bank::load_question_definition_classes('lti');
-        $q = new qtype_lti_question();
-        test_question_maker::initialise_a_question($q);
-        $q->name = 'Please write a code.';
-        $q->questiontext = 'Please write a code.';
-        $q->status = \core_question\local\bank\question_version_status::QUESTION_STATUS_READY;
-        $q->generalfeedback = 'I hope your code had a beginning, a middle and an end.';
-        $q->questiontextformat = FORMAT_HTML;
-        $q->generalfeedback = "Generalfeedback: Writing a code isn't to hard";
-        $q->generalfeedbackformat = FORMAT_HTML;
-        $q->defaultmark = 1;
-        $q->penalty = 0.3333333;
-        $q->qtype = 'lti';
-        $q->length = '1';
-        $q->hidden = '0';
-        $q->createdby = '2';
-        $q->modifiedby = '2';
-
-        $q->questionid = 1;
-        $q->unittest = 1;
-        $q->id = 1;
-        $q->course = 1;
-        $q->typeid = null;
-        $q->toolurl = 'https://tool.org';
-        $q->securetoolurl = 'https://tool.org';
-        $q->instructorchoicesendname = 1;
-        $q->instructorchoicesendemailaddr = 1;
-        $q->instructorchoiceallowroster = 1;
-        $q->instructorchoiceallowsetting = 1;
-        $q->instructorcustomparameters = 1;
-        $q->instructorchoiceacceptgrades = 1;
-        $q->grade = 1;
-        $q->launchcontainer = 1;
-        $q->resourcekey = 'key';
-        $q->password = 'secret';
-        $q->debuglaunch = 0;
-        $q->showtitlelaunch = 0;
-        $q->showdescriptionlaunch = 0;
-        $q->servicesalt = 'salt';
-        $q->instancecode = 'somerandominstancecode';
-        $q->originalinstancecode = null;
-        $q->icon = '';
-        $q->secureicon = '';
-
-        $q->qtype = question_bank::get_qtype('lti');
-
-        return $q;
-    }
-
-    /**
-     * Makes an lti question using plain text input.
-     *
-     * @return qtype_lti_question
-     */
-    public function make_lti_question_plain() {
-        $q = $this->initialise_lti_question();
-        return $q;
-    }
-
-    /**
      * Make the data what would be received from the editing form for an lti
      * question.
      *
@@ -108,13 +43,18 @@ class qtype_lti_test_helper extends question_test_helper {
      */
     public function get_lti_question_form_data_plain() {
         $q = new stdClass();
-        $q->name = 'Please write a code.';
+        $q->name = 'lti-question';
         $q->status = \core_question\local\bank\question_version_status::QUESTION_STATUS_READY;
-        $q->questiontext = 'Please write a code.';
-        $q->generalfeedback = 'I hope your code had a beginning, a middle and an end.';
-        $q->questiontextformat = FORMAT_HTML;
-        $q->generalfeedback = "Generalfeedback: Writing a code isn't to hard";
-        $q->generalfeedbackformat = FORMAT_HTML;
+        $q->questiontext = array(
+            "text" => 'Questiontext for Question 1',
+            'format' => FORMAT_HTML,
+            'itemid' => 1
+        );
+        $q->generalfeedback = array(
+            "text" => 'I hope your code had a beginning, a middle and an end.',
+            'format' => FORMAT_HTML,
+            'itemid' => 2
+        );
         $q->defaultmark = 1;
         $q->penalty = 0.3333333;
         $q->qtype = 'lti';
@@ -122,7 +62,6 @@ class qtype_lti_test_helper extends question_test_helper {
         $q->hidden = '0';
         $q->createdby = '2';
         $q->modifiedby = '2';
-
         $q->questionid = 1;
         $q->unittest = 1;
         $q->course = 1;
