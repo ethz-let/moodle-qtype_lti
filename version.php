569e7ec1e203a8a084ebf6510a1b05ceafab3895
Nexterday <nexterday@users.noreply.github.com>
2025-05-21 15:00:46 +0200

    Prepare for MOODLE_500_STABLE

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_lti';
-$plugin->version = 2025050600;
+$plugin->version = 2025052100;
 $plugin->requires = 2022041910; // Moodle 4.0.
 
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '4.5';
+$plugin->release = '5.0';
